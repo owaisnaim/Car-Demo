4c464ad05d3b1c486c185cfc10120b96402e46dc
Owais Naim <owaisnaim9@gmail.com>
2023-05-31 14:42:02 +0530

    Updated EndPoints.

--- src/main/java/com/example/demo/controller/CarController.java
+++ src/main/java/com/example/demo/controller/CarController.java
@@ -6,29 +6,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 @RestController
+@RequestMapping("/car")
 public class CarController {
     @Autowired
     private CarService carService;
-    @PostMapping("/addCar")
-    public Car addCar(@RequestBody Car car){
+
+    @PostMapping("/add")
+    public Car addCar(@RequestBody Car car) {
         return carService.saveCar(car);
     }
+
     @GetMapping("/cars")
-    public List<Car> cars(){
+    public List<Car> cars() {
         return carService.findAll();
     }
 
-    @GetMapping("/car/{id}")
-    public Car getCar(@PathVariable Integer id){
+    @GetMapping("/{id}")
+    public Car getCar(@PathVariable Integer id) {
         return carService.getCarById(id);
     }
+
     @PutMapping("/update")
-    public Car updateCar(@RequestBody Car car){
+    public Car updateCar(@RequestBody Car car) {
         return carService.updateCar(car);
     }
+
     @DeleteMapping("/delete/{id}")
-    public Car deleteCar(@PathVariable Integer id){
+    public Car deleteCar(@PathVariable Integer id) {
         return carService.deleteCar(id);
     }
 }
